64eb5cd25296f2ba13c4cdff24dfc1d4cb454dde
mkg20001 <mkg20001@gmail.com>
2016-03-20 13:24:51 +0100

    Add tests for Everything

--- test/index.js
+++ test/index.js
@@ -2,18 +2,92 @@ const config=require("../testconfig.js");
 const cc=require("../lib");
 describe("teamspeak-simple",function() {
   var sq;
-  it("should connect",function(done) {
+  it("connect",function(done) {
     new cc(config.host,config.login,function(e,c) {
       sq=c;
       done(e);
     });
   });
-  describe("server",function() {
+  describe("servers",function() {
     var s;
-    it("should get server object", function(done) {
+    it("create(name,cb)",function(done) {
+      sq.servers.create("Test",done);
+    });
+    it("servers",function() {
+      if (!sq.servers.servers) throw "Property Missing";
+    });
+    it("serversbyId",function() {
+      if (!sq.servers.serversbyId) throw "Property Missing";
+    });
+    it("stop(sid,cb)",function(done) {
+      sq.servers.stop(config.server,done);
+    });
+    it("start(sid,cb)",function(done) {
+      sq.servers.start(config.server,done);
+    });
+    it("state(sid,cb)",function(done) {
+      sq.servers.state(config.server,done);
+    });
+    it("delete(sid,cb)",function(done) {
+      sq.servers.delete(config.server,done);
+    });
+    it("getId(id)", function() {
+      if (!sq.servers.getId(config.server)) throw "Fail";
+    });
+    it("getId(id,cb)", function(done) {
       sq.servers.getId(config.server,function(e,serv) {
         s=serv;
         done(e);
+      });
+    });
+    describe("server", function() {
+      describe("server.users", function() {
+        it("users", function() {
+          if (!s.users.users) throw "Property Missing";
+        });
+        it("usersbyId", function() {
+          if (!s.users.usersbyId) throw "Property Missing";
+        });
+        it("getId(id)", function() {
+          if (!s.users.getId(s.users.users[0].uid)) throw "Fail";
+        });
+        it("getId(id,cb)", function(done) {
+          s.users.getId(s.users.users[0].uid,function(e,r) {
+            done(e);
+          });
+        });
+      });
+      describe("server.channels", function() {
+        it("channels", function() {
+          if (!s.channels.channels) throw "Property Missing";
+        });
+        it("channelsbyId", function() {
+          if (!s.channels.channelsbyId) throw "Property Missing";
+        });
+        it("getId(id)", function() {
+          if (!s.channels.getId(s.channels.channels[0].uid)) throw "Fail";
+        });
+        it("getId(id,cb)", function(done) {
+          s.channels.getId(s.channels.channels[0].uid,function(e,r) {
+            done(e);
+          });
+        });
+      });
+      describe("server.groups", function() {
+        it("groups", function() {
+          if (!s.groups.groups) throw "Property Missing";
+        });
+        it("groupsbyId", function() {
+          if (!s.groups.groupsbyId) throw "Property Missing";
+        });
+        it("getId(id)", function() {
+          if (!s.groups.getId(s.groups.groups[0].uid)) throw "Fail";
+        });
+        it("getId(id,cb)", function(done) {
+          s.groups.getId(s.groups.groups[0].uid,function(e,r) {
+            done(e);
+          });
+        });
       });
     });
   });
