fef3b25d91b0d57d25bffaf0a656cf0f189c2efe
mkg20001 <mkg20001@gmail.com>
2016-03-20 12:50:34 +0100

    Code Update

--- test/index.js
+++ test/index.js
@@ -1,11 +1,20 @@
 const config=require("../testconfig.js");
 const cc=require("../lib");
 describe("teamspeak-simple",function() {
-  var s;
+  var sq;
   it("should connect",function(done) {
     new cc(config.host,config.login,function(e,c) {
-      s=c;
+      sq=c;
       done(e);
+    });
+  });
+  describe("server",function() {
+    var s;
+    it("should get server object", function(done) {
+      sq.servers.getId(config.server,function(e,serv) {
+        s=serv;
+        done(e);
+      });
     });
   });
 });
